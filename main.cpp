a5e5de17e7df6f8f5a4f38f7f1c4354aa45c9189
alex-mat-s <59513334+alex-mat-s@users.noreply.github.com>
2020-01-06 17:38:01 +0500

    Считывание весов из файла
    Матрица весов считывается из файла .csv в матрицу, представляющую вектор векторов (аналог двумерного массива)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <QCoreApplication>
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,7 +12,49 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    cout << "Hello world!";
+    //w - матрица весов
+    vector< vector<double> > w;
 
-    return a.exec();
+    ifstream in("D:\\University\\DW\\DW\\debug\\weights.csv");
+
+    if(in){
+        cout << "Open file: success" << endl;
+
+        string line, field;
+        vector<double> v; //вектор для одной считанной строки
+
+        while (getline(in, line)){
+            v.clear();
+            stringstream ss(line);
+
+            while (getline(ss, field, ';'))  //разбить считанную строку по ;
+                v.push_back(stod(field));
+
+            w.push_back(v); //добавление вектора считанной строки в матрицу весов
+       }
+
+        //вывод считанной матрицы весов
+        for (size_t i = 0; i < w.size(); ++i) {
+            for (size_t j = 0; j < w[i].size(); j++)
+                cout << w[i][j] << " ";
+            cout << "\n";
+       }
+
+        in.close();
+    } else {
+        cout << "Open file: error!" << endl;
+        return -1;
+    }
+
+
+
+
+
+
+    //освобождение памяти
+    /*for (int i = 0; i < wrow; i++)
+        delete [] w[i];*/
+
+    system("pause");
+    return 0;
 }
